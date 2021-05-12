9f81799abad1624075b11d84cd834a234751bf61
Alex Cortez <acim650@gmail.com>
2021-05-12 11:40:57 -0700

    src/components/Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css';
 
 function Cards() {
     return (
@@ -6,11 +8,32 @@ function Cards() {
             <h1>Check out these EPIC destinations!</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
-                    <ul className='cards__items'></ul>
+                    <ul className='cards__items'>
+                        <CardItem 
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem 
+                            src='images/img-2.jpg'
+                            text='Travel through the islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                        <CardItem 
+                            src='images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adventure'
+                            path='/services'
+                        />
+                    </ul>
                 </div>
             </div>
         </div>
     )
 }
 
+
 export default Cards
+
